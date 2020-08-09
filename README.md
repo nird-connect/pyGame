9658276ae50bec62a7b09d1d4ff340de85e5656f
iDroid-nird <nirdteq@gmail.com>
2020-08-09 06:30:00 -0800

    files Updated can start project now

---
+++ README.md
@@ -0,0 +1,33 @@
+# pyGame
+
+Python Game Development
+Do well to paste work description in the readme area.
+Make it appealing by using short dashes to separate descriptions.
+
+-------------------------------------------------
+
+CEO: Profnird,
+Co-founder : Charles,
+developer : YOURNAMEHERE
+
+-------------------------------------------------
+
+Technologies : Languages and frameworks in use. list them with comma
+
+--------------------------------------------------
+
+---------------------------
+
+descriptions
+
+---------------------------
+
+--------------------------------------------------
+
+---------------------------
+
+program features
+
+---------------------------
+
+--------------------------------------------------
---
+++ main.py
@@ -0,0 +1,25 @@
+# Game codes here -- @nirdteq
+#ceo: profnird
+#co-founder: charles
+#Developer: YourNameHere
+#resources & reference : https://pygame-zero.readthedocs.io/en/stable/
+#color RGB reference : colorspire.com/rgb-color-wheel/
+#-----------------------------------------------------------
+print("Welcome")
+print("Do well to leave comment on every stage to help others")
+print("files management is very important,\n Your project will be used later times")
+print("Be the master of the craft solve problems and obey guidance too")
+
+#GameWindow is created automatically.
+#Discuss your interface with your guide(nird) and partner.
+#Get images,files and game description in order and set before you start dev.
+#------------------------------------------------------------------------
+
+#Start developing Game from Here
+
+#this is a basic window design.
+WIDTH = 500
+HEIGHT = 500
+#this creates a background-color of yellow.
+def draw():
+    screen.fill((225, 222, 0))
